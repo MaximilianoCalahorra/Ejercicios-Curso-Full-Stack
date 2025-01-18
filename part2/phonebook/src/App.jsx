566f4c6c719132150ac35bfc47f1861bb4abb126
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-01-18 18:19:16 -0300

    Resolución del ejercicio 8 de la parte 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
-    { name: 'Ada Lovelace' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
     
     let i = 0;
@@ -23,11 +28,13 @@ const App = () => {
     if(!isPresent)
     {
         const personObject = {
-          name: newName
+          name: newName,
+          number: newNumber
         }
       
         setPersons(persons.concat(personObject))
         setNewName('')
+        setNewNumber('')
     }
     else
     {
@@ -38,16 +45,15 @@ const App = () => {
   return (
     <>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
+      <form onSubmit={addPerson}>
+        <div>name: <input value={newName} onChange={handleNameChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </>
   )
 }
