0bc02ae2895868793b77b36bae3e8c8374d7198a
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-01-18 11:22:05 -0300

    Resolución del ejercicio 4 de la parte 2

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -14,29 +14,52 @@ const Course = ({course}) =>
 const Total = ({parts}) => <strong>total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</strong>
 
 const App = () => {
-const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
+const courses = [
     {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
+        id: 1,
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10,
+                id: 1
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7,
+                id: 2
+            },
+            {
+                name: 'State of a component',
+                exercises: 14,
+                id: 3
+            },
+            {
+                name: 'Redux',
+                exercises: 11,
+                id: 4
+            }
+        ]
     },
     {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-    },
-    {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
+        name: 'Node.js',
+        id: 2,
+        parts: [
+            {
+            name: 'Routing',
+            exercises: 3,
+            id: 1
+            },
+            {
+            name: 'Middlewares',
+            exercises: 7,
+            id: 2
+            }
+        ]
     }
-    ]
-  }
+]
 
-  return <Course course={course}/>
+    return <>{courses.map(course => <Course key={course.id} course={course}/>)}</>
 }
 
 export default App
