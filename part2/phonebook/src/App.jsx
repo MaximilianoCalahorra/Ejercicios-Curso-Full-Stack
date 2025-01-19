36cefef344a381eff28f209a9c3aa7e45ce61968
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-01-19 18:33:32 -0300

    Resolución del ejercicio 11 de la parte 2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({value, handleOnChange}) => 
 <div>filter shown with<input value={value} onChange={handleOnChange}/></div>
@@ -22,12 +23,13 @@ const Person = ({person}) => <p>{person.name} {person.number}</p>
 const Persons = ({persons}) => <>{persons.map(person => <Person key={person.name} person={person}/>)}</>
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
