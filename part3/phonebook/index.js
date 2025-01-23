44a53d6da87a7b5978bbfc64c5efe8c5e60e56a1
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-01-23 15:44:28 -0300

    Resolución del ejercicio 1 de la parte 3

---
+++ part3/phonebook/index.js
@@ -0,0 +1,40 @@
+//Obtenemos el servidor:
+const express = require('express')
+
+//Lo ponemos en funcionamiento:
+const app = express()
+
+//Datos de las personas:
+let persons =
+[
+    { 
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
+    },
+    { 
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+    },
+    { 
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+    },
+    { 
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+    }
+]
+
+//Obtener personas:
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
