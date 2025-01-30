3ea37780ea69fddcca17ded13b4a88729bb6f543
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-01-30 14:34:04 -0300

    Resolución del ejercicio 5 de la parte 4

--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -6,7 +6,17 @@ const totalLikes = blogs => {
   return blogs.reduce((total, blog) => total + blog.likes, 0)
 }
 
+const favoriteBlog = blogs => {
+  const moreLikesBlog = blogs.reduce((max, currentItem) => currentItem.likes > max.likes ? currentItem : max, blogs[0])
+  return {
+    title: moreLikesBlog.title,
+    author: moreLikesBlog.author,
+    likes: moreLikesBlog.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
