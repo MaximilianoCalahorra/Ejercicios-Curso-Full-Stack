1b8b26fdd1003f73026ec6e701fe92241319645a
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-01-31 17:46:10 -0300

    Resolución del ejercicio 8 de la parte 4

---
+++ part4/blogs/tests/blog_api.test.js
@@ -0,0 +1,32 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for(let blog of helper.initialBlogs)
+  {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
+test('all blogs are returned as json', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
