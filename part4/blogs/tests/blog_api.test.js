52efc5638a4a8e9d3d864c5feb08570fe117648c
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-02-01 10:54:07 -0300

    Resolución del ejercicio 9 de la parte 4

--- part4/blogs/tests/blog_api.test.js
+++ part4/blogs/tests/blog_api.test.js
@@ -27,6 +27,13 @@ test('all blogs are returned as json', async () => {
   assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
+test('property name must be "id"', async () => {
+  const response = await api
+    .get('/api/blogs')
+  assert.strictEqual(response.body[0]._id, undefined)
+  assert.strictEqual(typeof response.body[0].id, 'string')
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
