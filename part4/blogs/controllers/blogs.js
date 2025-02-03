bee5a7525c166c62f2e6998e8b715f7b62083258
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-02-03 11:23:32 -0300

    Resolución del ejercicio 19 de la parte 4

--- part4/blogs/controllers/blogs.js
+++ part4/blogs/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -9,16 +10,28 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+
   if(!body.title || !body.url)
   {
     return response.status(400).end()
   }
-
-  const users = await User.find({})
-  const index = Math.round(Math.random() * users.length)
-  const user = users[index]
 
   const blog = new Blog({
     title: body.title,
--- part4/blogs/utils/middleware.js
+++ part4/blogs/utils/middleware.js
@@ -37,6 +37,11 @@ const errorHandler = (error, request, response, next) => {
     })
   }
 
+  if(error.name ===  'JsonWebTokenError')
+  {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
   next(error)
 }
 
