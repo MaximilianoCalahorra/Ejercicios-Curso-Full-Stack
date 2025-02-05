8816ccfd92814c128b9c9646d605e785ec793e96
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-02-05 20:46:02 -0300

    Resolución del ejercicio 1 de la parte 5

--- part5/blogs/bloglist-frontend/src/App.jsx
+++ part5/blogs/bloglist-frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
+import Blogs from './components/Blogs'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,14 +16,32 @@ const App = () => {
     )  
   }, [])
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try 
+    {
+      const user = await loginService.login({username, password})
+      
+      blogService.setToken(user.token)
+      
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } 
+    catch(exception) 
+    {
+      console.log('Wrong credentials')
+    }
+  }
+
+  if(user === null)
+  {
+    return <LoginForm username={username} password={password} handleLogin={handleLogin} 
+                      setUsername={setUsername} setPassword={setPassword}/>
+  }
+
+  return <Blogs blogs={blogs}/>
 }
 
 export default App
