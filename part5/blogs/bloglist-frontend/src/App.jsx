14912edab996ce8c2e48dd3040f05a16f3196c80
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-02-10 13:22:48 -0300

    Resolución del ejercicio 2 de la parte 5

--- part5/blogs/bloglist-frontend/src/App.jsx
+++ part5/blogs/bloglist-frontend/src/App.jsx
@@ -16,6 +16,16 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if(loggedUserJSON) 
+    {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -23,6 +33,10 @@ const App = () => {
     {
       const user = await loginService.login({username, password})
       
+      window.localStorage.setItem(
+        'loggedUser', JSON.stringify(user)
+      )
+
       blogService.setToken(user.token)
       
       setUser(user)
@@ -35,13 +49,18 @@ const App = () => {
     }
   }
 
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedUser')
+    setUser(null)
+  }
+
   if(user === null)
   {
     return <LoginForm username={username} password={password} handleLogin={handleLogin} 
                       setUsername={setUsername} setPassword={setPassword}/>
   }
 
-  return <Blogs blogs={blogs}/>
+  return <Blogs blogs={blogs} user={user} handleLogout={handleLogout}/>
 }
 
 export default App
